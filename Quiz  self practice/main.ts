7c893791e5bc89faf22cf7ae3a5ceec855cef7c7
Hafiza Yousra Khan <159699912+yousrakhan23@users.noreply.github.com>
2024-07-14 15:24:51 +0500

    Add files via upload

---
+++ Quiz  self practice/main.ts
@@ -0,0 +1,479 @@
+// let number = 10<=5
+// console.log(number);
+
+// let number = 5 > 3 && 10 > 20
+// console.log(number);
+
+// let number = 10 + 5 * 2
+// console.log(number);
+
+// let check = (`Hello` + `World`);
+// console.log(check);
+
+// let num = 3 / 0 ;
+// console.log(num);
+
+// let num = 2 ** 3 
+// console.log(num);
+
+// let num1 : number = 16;
+// let num2 : number = 7;
+// let result : number = num1 % num2 ;
+// console.log(result);
+
+// let x : number = 10 ;
+// x+= 2 ;
+// console.log(x);
+
+// let num = 5 !== 5 
+// console.log(num);
+
+// let num1 : number = 5;
+// let num2 : string = `5`;
+// console.log(num1 === num2);
+
+
+// function add (a: number , b: number): number {return a + b; } const result: number = add(1,2); 
+// const errorResult : number = ("1" , 2);
+
+// const message : string = "Hello, world";
+// const inferredMessage = "Hello, world!";
+// function add (a: number , b: number): number {return a + b;}
+// const result = add(1,2);
+// console.log(message);
+// console.log(inferredMessage);
+// console.log(result);
+
+// let myObject : object = {name:"John", age:30, isStudent: true};
+// console.log(myObject);
+
+// enum Color {RED, GREEN, BLUE}
+// let myColor: Color = Color.GREEN;
+// console.log(myColor);
+
+// function identity <T> (arg:T):
+// T,T {return arg;}
+// let result = identity<string>("Hello, generics!");
+// console.log(result);
+// let anotherResult = identity<number>(42);
+// console.log(anotherResult);
+
+// class MyClass { private privateProperty : string = "Private Property"; private privateMethod(): void {console.log("?");}}
+// const myInstance = new MyClass();
+// console.log(myInstance.privateProperty); myInstance.privateMethod();
+
+// const user = {
+//     firstName: "Angela",
+//     lastName: "Davis",
+//     role: "Professor",
+// }
+// console.log(user.name)
+
+// import express from "express"
+// const app = express()
+// app.get ("/", function(req,res){
+//     res.sen 
+//     sendsend
+//     Datesend
+//     filesend
+//     FilesendStatus
+// })
+// app.listen(3000)
+
+// interface User {
+//     id: number
+//     firstName: string
+//     lastName: string
+//     role:string
+// }
+// function updateUser(id:number, update:Partial<User>){
+//     const user = getUser(id)
+//     const newUser = { ...user, ...update }
+//     saveUser(id, newUser)
+// }
+
+// let helloWorld = "Hello World";
+// let helloWorld:string
+
+// const user = {
+//     name: "Hayes",
+//     id: 0,
+//     };
+    
+//     "use strict";
+//     const user = {
+//         name: "Hayes",
+//         id: 0,
+//     };
+  
+// interface User {  }
+// name:string;
+// id:number;
+// }
+
+// if (true){
+//     let a = 4;
+   
+// }
+// else {
+// let a = "string";
+
+// }
+
+// console.log(a);
+
+// Var a: number = "7";
+//     a = 9;
+ // console.log(a);
+
+// let myType: any = {name: "Nida", id: 1};
+// myType = {id: 2, name: "Ibad"};
+// myType = {id: 3, name: "Raffay", gender: false};
+// myType = {id: 4, gender: false };
+
+// const a = 5;
+// a++;
+// console.log(a);
+
+// let notSure: any = 4;
+//            notSure = "maybe a string instead";
+//             notSure = false;
+
+// const enum Color1 {Red=1, Green, Blue};
+// var colorName: String = Color[2];
+//  console.log(colorName);
+
+// Var x: any "Tom"; 
+// if(typeof x === "string"){
+//      console.log(x.legnth);
+// }
+// x.unknown();
+
+
+
+// Var a: { too: number };
+//            Var a1 = { too: 1, baz: 2 };
+//            a = a1;
+
+
+// let c = ((a,b) => ++a)(1, 2);
+// console.log(c);
+
+// let myType = {name: "Nida", id: 1 };
+// let myType1 = { id: 2, name: "Tom", age: 22  };
+// myType = myType1;
+
+
+
+
+// let arr=[10,20,30,40];for (var val of arr) {
+//     console.log(val);
+// }
+// import chalk from 'chalk' ; 
+// console.log(chalk.bold('Yousra'));
+
+// console.log(10 <= 5) ;
+// let array= [1 , 2 , 3]
+// Array[0]
+// Array.first
+
+
+// let myFunction : (p: string) => number;
+
+// function myFunction(){}
+
+// const myFunction = () => {}
+
+// let greeting:string="Hello World" ; 
+// console.log(greeting);
+
+// function myFunction(p:string = "default") {}
+
+
+
+// let isDone: boolean = true ;
+// let total: number = 100 ;
+// let name1: string = "John" ;
+// let something: number = "100" ;
+
+// let greeting: string = "Hello World" ;
+// console.log(greeting) ;
+
+// let message = "Hello World" ;
+// console.log(message);
+
+// let message ;
+// console.log(message);
+
+// let name = "kanwal" ;
+// console.log("Name") ;
+
+// let age = 19 ; // Q11
+// age = "kanwal" ;
+
+// let fruit = "mango" ; // Q12
+// fruit = "orange" ;
+// console.log(fruit) ;
+
+// let isStudent = true ;
+// console.log(isStudent) ;
+
+// let education = {
+//     class : "2nd year" ,
+//     subject : "pre medical"
+// }
+// console.log(education.key) ;
+
+// let object = {
+//     name : "yousra" ,
+//     status : "single" ,
+// }
+// let jsonString = JSON.stringify(object) ;
+// console.log(jsonString) ;
+
+
+// const jsonString = {
+//     "name" : "yousra",
+//     "status" : "single"
+// } ;
+// const object = JSON.parse(jsonString); // error aiga qk hamy isko lazmi javascript may krna hy 
+// console.log(object) ;
+
+
+// let message = Hello World ; // syntax error
+// console.log(message) ;
+
+
+
+// let message = "Hello World" ;
+// console.loger(message) ;     // type error
+
+// const x = 10
+// console.log(x)
+
+// let result= x+5
+// console.log(result);
+
+// let number = 10 ;
+// console.log("Hello World") ;
+// if (true {
+//     console.log("True");
+// })
+// const pi = 3.14;
+
+// let num:any= "Hello" ;
+
+// let list : number[] = [1 , 2 , "3"]// type error
+
+// let person: { name: string, age: number } = { name: "John", age: "25" };
+
+// function greet (name: string) : void{
+// console.log("Hello , " + name) ;
+// }
+// greet(123) ;
+
+
+// let array = [1 , 2 , 3] ;
+// array.push("four");
+// console.log(array);
+
+// let result = "5" + 5;
+// console.log(result);
+
+
+// let count:number = 5 ;
+// let result: string = typeof count === "number"? count.toString():NaN;
+
+// let message = "Hello World" ;
+// message = 9 ;
+// console.log(message) ;
+
+// let x : number = "Hello" ;
+
+// let num : number = 76 ;
+// num = "Ans"
+
+// let value : boolean = false ;
+// value = 2;
+
+// let result : string = "Hello" ;
+// result = 100;
+
+// let age: number;
+// age = "twenty-five";
+
+// const result = (5 > 3) && (3 < 2);
+// console.log(result) ;
+
+// const result = (undefined || 'default');  
+// console.log(result) ;
+
+// let day = 2;
+
+// switch (day) {
+
+//     case 1:
+
+//         console.log("Monday");
+
+//         break;
+
+//     case 2:
+
+//         console.log("Tuesday");
+
+//         break;
+
+//     case 3:
+
+//         console.log("Wednesday");
+
+//         break;
+
+//     default:
+
+//         console.log("Invalid day");
+
+// }  // tuesday
+
+// let number = 15;
+
+// let message = number % 2 === 0 ? "Even" : "Odd";
+
+// console.log(message); // odd
+
+
+// let num = 10;
+
+// let str = "10";
+
+// if (num === str) {     // error
+
+//     console.log("Equal");
+
+// } else {
+
+//     console.log("Not Equal");
+
+//}
+
+// let isAvailable = false;
+
+// console.log(!isAvailable); 
+
+// let a = 5;
+
+//  let b = 10;
+
+// let result = (a > 0 && b++ > 5) && (a++ > 0);
+
+// console.log(`result: ${result}, a: ${a}, b: ${b}`);
+
+
+
+
+// let str: string = "hello";
+
+// if (str === "HELLO") {
+
+//     console.log("Exact match");
+
+// } else if (str.toLowerCase() === "hello") {
+
+//     console.log("Case-insensitive match");
+
+// } else {
+
+//     console.log("No match");
+
+// }
+
+// let x = 10;
+
+// let y = 5;
+
+// if (x > 5 && y < 10) {
+
+//     console.log("Condition is true");
+
+// } else {
+
+//     console.log("Condition is false");
+
+// }
+
+// const k = (5 > 3) ? 'Yes' : 'No';
+
+// const l = (5 < 3) ? 'Yes' : (3 === 3) ? 'Maybe' : 'No';
+
+// console.log(k, l);
+
+// let x = true;
+
+// let y = false;
+// (x && y) || (!x && !y)
+
+// let count : number = 5 ;
+// let result:string = typeof count==="number" ? count.toString():"NaN" ;
+// console.log(result);
+
+// const PI : number = 3.14;
+// PI= 3.14150;
+// console.log(PI) ;
+
+// let numbers : number[] = [1,2,3,4];
+// let firstNumber = numbers("0");
+
+// let numbers: number[] = [1, 2, 3, 4];
+// let firstNumber = numbers[0];
+// console.log(firstNumber); // Outputs: 1
+
+// let grades : number[] = [70 , 85,90];
+// let lastGrade = grades[grades.length-1];
+// console.log(lastGrade);
+
+
+// let direction : "up" | "down" | "left" |"right" ;
+// direction="up" ;
+// console.log(direction);
+
+
+// function add(a:number , b:number) :number{
+//     return a+b;
+// }
+// console.log(add) ;
+
+// function greet (name:string , greeting?: string):void{
+//     console.log(`${greeting} , ${name}`);
+// }
+
+// function greet(name:string , greeting:string="Hello"):void{
+//     console.log(`${greeting} , ${name}`);
+// }
+
+// const add =(a:number , b:number):number=> a+b
+// console.log(add(5 , 3)); // 8
+
+// for (let i = 0; i< 5; i++){
+// console.log(i) ;
+// }
+
+// let j = 0;
+// while(j<5){
+//     console.log(j);
+//     j++;
+// }
+
+// let k = 0;
+// do{
+//     console.log(k);
+//     k++;
+// }while(k<5);
+
+const user = {
+    name:"coco",
+    age: 24
+};
+for (let key in user){
+    console.log(`${key}:${user[key]}`);
+}
